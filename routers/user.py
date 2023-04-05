47bd94177b19047baa2af2853e54c499e2f5240d
Maksim Patsulo <36102743+IloveCandies@users.noreply.github.com>
2023-04-05 13:56:53 +0300

    add shemas
    Добавил схемы

--- routers/user.py
+++ routers/user.py
@@ -6,8 +6,8 @@ router = APIRouter()
 
 @router.post("/add_to_favorite_list/")
 async def add_to_favorite_list(user_id:str, group_id:int) -> Student:
-    user =  [x for x in Students if x.id ==user_id ][0]
-    group = [x for x in Groups if x.id ==group_id ][0]
+    user =  [x for x in Students if x.id == user_id ][0]
+    group = [x for x in Groups if x.id == group_id ][0]
     user.favorite_list.groups.append(group)
     return user
 
--- shemas.py
+++ shemas.py
@@ -1,15 +1,82 @@
+import datetime
 from typing import Union, List
 from uuid import uuid4
 from enum import Enum
-from fastapi import FastAPI
 from dataclasses import dataclass
 from datetime import date
 from pydantic import BaseModel
+
 class Education_level(Enum):
     Специалитет = 1
     Бакалавриат = 2
     Магистратура =3
+
+class Education_form(Enum):
+    Очная = 1
+    Заочная = 2
+    Очно-заочная = 3
+
+
+@dataclass
+class Discipline(BaseModel):
+    name:str
+
+@dataclass
+#как назвать подумать
+class OOP(BaseModel):
+    code: str
+    direction: str
+    eduction_profile: str
+    education_level:Education_level
     
+
+@dataclass
+class ShedulePlan(BaseModel):
+    recruitment_year:datetime.date
+    oop:OOP
+    form:Education_form
+    period:int
+
+@dataclass
+class DisciplinesInShedulePlan(BaseModel):
+    hours:int
+    zet: int
+    education_form: Education_form
+  
+@dataclass
+class DisciplinesInStudentEducation(BaseModel):
+    hours:int
+    zet: int
+    education_form: Education_form
+
+@dataclass
+class StudentEducation(BaseModel):
+    level:Education_level
+    oop:OOP
+    form:Education_form
+    education_end: bool
+    date_of_end_education: datetime.date
+    disciplines = List[Discipline]
+
+@dataclass
+class Region(BaseModel):
+    number:int
+    name:str
+
+@dataclass
+class Sity(BaseModel):
+    region: Region
+    name:str
+
+@dataclass
+class Education(BaseModel):
+    level:Education_level
+    oop:object
+    form:object
+    education_end: bool
+    date_of_end_education: datetime.date
+
+
 @dataclass
 class Place(BaseModel):
     id:int
@@ -24,6 +91,7 @@ class Place(BaseModel):
 class University(BaseModel):
     id:int
     name: str
+    sity: Sity
     description: Union[str, None] = None
     list_places: Union[List[Place],None] = None
     university_staff: Union[list, None] = None
@@ -37,9 +105,9 @@ class Group(BaseModel):
     potential_places:int
     course:int
     end_year:int
-    
+    shedule_plan:ShedulePlan
+
     def create(id,name,year_of_recruitment,available_places,potential_places,course,end_year):
-        
         return(Group(id = id,name=name,year_of_recruitment=year_of_recruitment,
                      available_places = available_places,potential_places=potential_places,
                      course=course,end_year=end_year))
@@ -51,7 +119,8 @@ class FavoriteList(BaseModel):
     
     def create(self,last_update):
         self.last_update = last_update
-    
+
+
 class Student(BaseModel):
     id:str
     first_name:str
@@ -61,18 +130,9 @@ class Student(BaseModel):
     email:str = "default@mail.com"
     contact_email:str ="fault@mail.com" 
     password:str
+    sity:Sity
+    education:Union[List[StudentEducation],None] = None
     favorite_list: FavoriteList = FavoriteList(last_update=date.today().strftime('%Y-%m-%d'),comment="dddd")
     
     def create(first_name,middle_name,last_name):
         return Student(id = "ц",first_name=first_name,middle_name=middle_name,last_name=last_name, password="None")
-
-    
-class StudentShema(BaseModel):
-    id:str 
-    first_name:str
-    middle_name:str
-    last_name:str
-    phone:str = "8-800-555-35-35"
-    email:str = "default@mail.com"
-    contact_email:str ="fault@mail.com" 
-    password:str
